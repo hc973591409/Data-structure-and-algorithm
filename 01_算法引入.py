4a826e8607f5a0a3498e77e12c4d3e0141028f43
hc973591409 <973591409@qq.com>
2018-09-08 10:52:00 +0800

    算法的引入，可以看到，注意执行时间 可以比较calc1 和calc的时间复杂度对执行时间的影响

--- 01_算法引入.py
+++ 01_算法引入.py
@@ -12,8 +12,22 @@ def calc_1():
 
     time_end = time.time()
     print("程序共耗时%s s" % (time_end-time_start))
-    # 程序共耗时148.1957812309265s
+    # 程序共耗时148.1957812309265s    时间复杂度O(N^3)
+
+
+def calc_2():
+    """修改程序，优化算法"""
+    time_start = time.time()
+    for a in range(0, 1001):
+        for b in range(0, 1001):
+            c = 1000 - a - b
+            if a**2 + b**2 == c**2:
+                print("%s + %s + %s = 1000" % (a, b, c))
+
+    time_end = time.time()
+    print("程序共耗时%.2f s" % (time_end-time_start))
+    # 程序共耗时程序共耗时1.12 s     时间复杂度O(N^2)
 
 
 if __name__ == '__main__':
-    calc_1()
+    calc_2()
