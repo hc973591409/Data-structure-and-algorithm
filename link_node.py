35df34a25e643882657c833b7ead275edc324aad
hc973591409 <973591409@qq.com>
2018-09-08 16:38:42 +0800

    单链表和双链表的节点类

--- link_node.py
+++ link_node.py
@@ -7,6 +7,16 @@ class Node:
         # 默认下个节点为空
 
 
+class DoubleNode:
+    """单向链表的节点类"""
+    def __init__(self, item):
+        """要插入的item数据"""
+        self.item = item
+        self.next = None
+        self.pre = None
+        # 默认下个节点为空
+
+
 if __name__ == '__main__':
-    node = Node(5)
-    print(node.item, node.next)
+    node = DoubleNode(5)
+    print(node.item, node.next, node.pre)
